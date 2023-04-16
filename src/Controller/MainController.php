4c38c5c1b4fc8a5ea2be00beae9155be830a290f
Martin Kamenov <D:\The Bat!>
2023-04-16 13:15:33 +0300

    initial commit symfony Udemy

---
+++ src/Controller/MainController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/main", name="app_main")
+     */
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+     /**
+     * @Route("/start/{name?}", name="start")
+     */
+    public function  start(Request $request, $name) {
+        return new Response('<h1>Hello '.$name.'!</h1>');
+
+    }
+}
